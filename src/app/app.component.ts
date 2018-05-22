066175dd2de495bc19be69236d689389b638d5d7
SPIDlukezilla <luka.vrataric@gmail.com>
2018-05-22 20:47:11 +0200

    search jumps, responsive changes, validation of twitch username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, AfterViewInit} from '@angular/core';
 import { Response } from '@angular/http';
 
 import { RouterModule } from '@angular/router';
@@ -7,15 +7,22 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Http, Headers } from '@angular/http';
 
-// import fontawesome, { FontawesomeObject } from '@fortawesome/fontawesome';
-// import faTrashAlt from '@fortawesome/fontawesome-free-regular/';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/interval';
+import { element } from 'protractor';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+
+  WindowWidth: number = window.innerWidth;
+
 
   FullTable: any;
   NewUser: string;
@@ -26,9 +33,9 @@ export class AppComponent {
   Platform = 'pc';
   SelectedPlatform = 'pc';
 
-  ServerResponse: string;
-
   Search: string;
+
+  FormValid = true;
 
   CurretnlyFeatured: string;
   FeaturedList = ['lifetimeKD', 'lifetimeKills', 'lifetimeWins', 'currentSeasonKDSolo', 'currentSeasonKDDuo', 'currentSeasonKDSquad'];
@@ -37,31 +44,57 @@ export class AppComponent {
   FeaturedItem: string;
   FeaturedItems: any;
   FeaturedPlatform: string;
+  FeaturedPlatformID: number;
+  id: number;
+  i: number;
 
   TwitchResponse: any;
   isOnline: boolean;
   liveCount: number;
 
+  showAlertError = false;
+  showAlertWarning = false;
+  showAlertSuccess = false;
+  showAlertNaP = false;
+
+  ServerResponse: string;
+
+  SubmitLoading = false;
+
   constructor(private http: HttpClient) {
     this.http.get('https://fortniteleaderboard.azurewebsites.net/Fortnite/FullTable?sortBy=1&platform=pc').subscribe(data => {
       this.FullTable = data;
-      this.checkIfLive(this.FullTable);
       console.log(data);
     });
-    const i = Math.floor(Math.random() * this.FeaturedListClean.length);
-    const id = i + 4;
-    this.http.get('https://fortniteleaderboard.azurewebsites.net/Fortnite/FullTable?sortBy=' + id + '&platform=pc').subscribe(data => {
-      this.CurrentlyFeaturedTable = data;
-      this.FeaturedItem = this.FeaturedList[i];
-      this.CurretnlyFeatured = this.FeaturedListClean[i];
-      console.log(this.CurrentlyFeaturedTable);
-      this.checkIfLive(this.CurrentlyFeaturedTable);
-    });
-    // this.CurretnlyFeatured = this.FeaturedListClean[i];
 
+    this.getFeaturedTable();
+
+    Observable.interval(2000 * 60).subscribe(x => {
+      this.getFeaturedTable();
+      console.log('Changed currently featured table');
+    });
+
+    Observable.interval(1000 * 60).subscribe(x => {
+      this.onUpdate();
+      console.log('Called update on full table');
+    });
   }
 
+  ngAfterViewInit() {
+    this.WindowWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  resize(event) {
+    this.WindowWidth = window.innerWidth;
+    console.log(this.WindowWidth);
+  }
+
+
   onSubmit(form: HTMLFormElement) {
+
+    this.SubmitLoading = true;
+
     this.NewUser = form.value.igralec;
     this.NewUserTwitch = form.value.twitch;
 
@@ -76,15 +109,27 @@ export class AppComponent {
       .subscribe(
         res => {
           console.log(res);
+          if (res === 'Username is already on the list') {
+            this.showAlertWarning = true;
+          } else if (res === 'User added') {
+            this.showAlertSuccess = true;
+          } else if (res === `User doesn't exist`) {
+            this.showAlertNaP = true;
+          } else {
+            this.showAlertError = true;
+            this.ServerResponse = res.toString();
+          }
+          this.SubmitLoading = false;
           this.http.get('https://fortniteleaderboard.azurewebsites.net/Fortnite/FullTable?sortBy=1'
             + '&platform=' + this.SelectedPlatform).subscribe(data => {
               this.FullTable = data;
-              this.checkIfLive(this.FullTable);
             });
           form.reset();
         },
         err => {
           console.log(err);
+          this.showAlertError = true;
+          this.ServerResponse = err.message;
         }
       );
   }
@@ -99,7 +144,6 @@ export class AppComponent {
       + type + '&platform=' + this.SelectedPlatform).subscribe(data => {
         this.FullTable = data;
         this.RatingType = type;
-        this.checkIfLive(this.FullTable);
       });
   }
 
@@ -109,43 +153,81 @@ export class AppComponent {
 
   onChangeTypePlatform(splat: string) {
     this.SelectedPlatform = splat;
+
     this.http.get('https://fortniteleaderboard.azurewebsites.net/Fortnite/FullTable?sortBy=1&platform=' + splat).subscribe(data => {
       this.FullTable = data;
-      this.checkIfLive(this.FullTable);
     });
   }
 
-  onUpdateTables() {
-    /*     this.http.get('https://fortniteleaderboard.azurewebsites.net/Fortnite/UpdateUsers?calledFromCron=true').subscribe(data => {
-          console.log(data);
-          this.http.get('https://fortniteleaderboard.azurewebsites.net/Fortnite/FullTable?sortBy=1'
-            + '&platform=' + this.SelectedPlatform).subscribe(table => {
-              this.FullTable = table;
-            });
-        }); */
-
+  onDissmiss(i: number) {
+    switch (i) {
+      case 0:
+        this.showAlertSuccess = false;
+        break;
+      case 1:
+        this.showAlertWarning = false;
+        break;
+      case 2:
+        this.showAlertError = false;
+        break;
+      case 3:
+        this.showAlertNaP = false;
+        break;
+    }
   }
 
-  checkIfLive(tabela: any) {
+  onUpdate() {
+    this.http.get('https://fortniteleaderboard.azurewebsites.net/Fortnite/FullTable?sortBy='
+      + this.RatingType + '&platform=' + this.SelectedPlatform).subscribe(data => {
+        this.FullTable = data;
+      });
+  }
 
-    let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.append('Client-ID', '66z5kiixkmb10wu5llchu5le0pna4k');
+  getCurrenltyFeatured() {
+    switch (this.FeaturedPlatformID) {
+      case 0:
+        this.FeaturedPlatform = 'pc';
+        break;
+      case 1:
+        this.FeaturedPlatform = 'psn';
+        break;
+      case 2:
+        this.FeaturedPlatform = 'xbl';
+        break;
+      default:
+        this.FeaturedPlatform = 'all';
+        break;
+    }
+  }
 
-    tabela.forEach(element => {
-      if (element.twitchUsername) {
-        const name = element.twitchUsername.slice(1);
-        this.http.get('https://api.twitch.tv/helix/streams?user_login=' + name, { headers: headers }).subscribe(res => {
-          this.TwitchResponse = res;
-          console.log(res);
-          if (this.TwitchResponse.data.length > 0) {
-            element['live'] = true;
-            element['liveCount'] = this.TwitchResponse.data[0].viewer_count;
-          } else {
-            element['live'] = false;
-          }
-        });
-      }
-    });
+  getFeaturedTable() {
+    this.http.get('https://fortniteleaderboard.azurewebsites.net/Fortnite/Spotlight')
+      .subscribe(data => {
+        console.log(data);
+        this.CurrentlyFeaturedTable = data['fortniteUsers'];
+        this.FeaturedPlatformID = data['platform'];
+        this.id = data['sortedBy'];
+        this.CurretnlyFeatured = this.FeaturedListClean[this.id];
+        this.FeaturedItem = this.FeaturedList[this.id];
+        this.getCurrenltyFeatured();
+      });
+  }
+
+  onSearchChange() {
+    const find = this.FullTable.find(x => x.username === this.Search);
+    if (find) {
+      const scroll = document.getElementById(this.Search);
+      // console.log(scroll);
+      scroll.scrollIntoView(false/* {block: 'end', behavior: 'smooth', inline: 'end'} */);
+    }
+  }
+
+  onTwitchChange() {
+    if (this.NewUserTwitch === '' || this.NewUserTwitch[0] === '/') {
+      this.FormValid = true;
+    } else {
+      this.FormValid = false;
+    }
   }
 
 }
